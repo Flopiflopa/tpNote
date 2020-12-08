a523f6e79d438539e79cba5df5905d9beeaae8c8
Flopiflopa <alexandre.verge2@gmail.com>
2020-12-08 09:41:15 +0100

    Ajout fichier code

---
+++ Bloc.java
@@ -0,0 +1,14 @@
+/*
+ * Bloc.java
+ * @author Alex
+ * @date 08 déc. 2020
+ */
+package formation1.sudoku;
+
+public class Bloc {
+	
+	int[] lignesBloc; // num de lignes du bloc
+	int[] colonnesBloc; // num de colonnes du bloc
+	
+	
+}
